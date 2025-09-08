ed99b1ebab8b4e4b6eb34bdcb23b50218155630c
[200~ <jfzl1994@gmail.com>
2025-09-08 16:26:15 -0500

    chore: Limpiar archivos temporales, scripts de testing y restos del sistema de tallas revertido

--- src/services/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Entry point for backend services
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./db"), exports);
-tslib_1.__exportStar(require("./userService"), exports);
-//# sourceMappingURL=index.js.map
